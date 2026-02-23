4d59bf53adb4396712f60a18b84d0fbe3ccf0f74
riteshthakre1 <riteshthakre976@gmail.com>
2026-02-23 14:52:15 +0530

    [Ritesh] : modified UC10-Generic-Quantity-Class-with-Unit-Interface-For-Multi-Category-Support LengthUnit.java file

--- src/main/java/com/apps/quantitymeasurement/LengthUnit.java
+++ src/main/java/com/apps/quantitymeasurement/LengthUnit.java
@@ -1,26 +1,27 @@
 package com.apps.quantitymeasurement;
 
-public enum LengthUnit {
+public enum LengthUnit implements Unit {
 
-    FEET(12.0),
-    INCHES(1.0),
-    YARDS(36.0),
-    CENTIMETERS(0.393701);
+	FEET(12.0), INCHES(1.0), YARDS(36.0), CENTIMETERS(0.393701);
 
-    private final double conversionFactor;
+	private final double conversionFactor; // base = inches
 
-    LengthUnit(double conversionFactor) {
-        this.conversionFactor = conversionFactor;
-    }
+	LengthUnit(double conversionFactor) {
+		this.conversionFactor = conversionFactor;
+	}
 
-    // Convert value to base unit (inches)
-    public double toBase(double value) {
-        return value * conversionFactor;
-    }
+	@Override
+	public double toBase(double value) {
+		return value * conversionFactor;
+	}
 
-    // Convert from base unit (inches) to this unit
-    public double fromBase(double baseValue) {
-        double result = baseValue / conversionFactor;
-        return Math.round(result * 1000000.0) / 1000000.0;
-    }
+	@Override
+	public double fromBase(double baseValue) {
+		return baseValue / conversionFactor;
+	}
+
+	@Override
+	public double getConversionFactor() {
+		return conversionFactor;
+	}
 }
