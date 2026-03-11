8d6a7eb9e29669d30fa8863a9ee279964c3d2cec
riteshthakre1 <riteshthakre976@gmail.com>
2026-03-11 11:39:21 +0530

    [Ritesh] : modified QuantityMeasurementApp.java

--- src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
+++ src/main/java/com/apps/quantitymeasurement/QuantityMeasurementApp.java
@@ -1,88 +1,44 @@
 package com.apps.quantitymeasurement;
 
-import java.util.Scanner;
-
 public class QuantityMeasurementApp {
 
-    // Equality for JUnit
-    public static boolean demonstrateLengthComparison(
-            double v1, Length.LengthUnit u1,
-            double v2, Length.LengthUnit u2) {
+	public static boolean demonstrateLengthComparison(double v1, Length.LengthUnit u1, double v2,
+			Length.LengthUnit u2) {
 
-        Length l1 = new Length(v1, u1);
-        Length l2 = new Length(v2, u2);
-        return l1.equals(l2);
-    }
+		Length l1 = new Length(v1, u1);
+		Length l2 = new Length(v2, u2);
 
-    // Conversion for JUnit
-    public static Length demonstrateLengthConversion(
-            double value,
-            Length.LengthUnit fromUnit,
-            Length.LengthUnit toUnit) {
+		return l1.equals(l2);
+	}
 
-        if (fromUnit == null || toUnit == null) {
-            throw new IllegalArgumentException("Units cannot be null");
-        }
+	public static Length demonstrateLengthConversion(double value, Length.LengthUnit fromUnit,
+			Length.LengthUnit toUnit) {
 
-        Length source = new Length(value, fromUnit);
-        return source.convertTo(toUnit);
-    }
+		Length source = new Length(value, fromUnit);
+		return source.convertTo(toUnit);
+	}
 
-    // -------- Console Methods Below --------
+	
+	public static Length demonstrateLengthConversion(Length length, Length.LengthUnit targetUnit) {
 
-    public static void demonstrateLengthComparison(Scanner sc) {
-        System.out.print("Enter first value: ");
-        double value1 = sc.nextDouble();
-        Length.LengthUnit unit1 = getUnit(sc, "first");
+		return length.convertTo(targetUnit);
+	}
 
-        System.out.print("Enter second value: ");
-        double value2 = sc.nextDouble();
-        Length.LengthUnit unit2 = getUnit(sc, "second");
+	public static void main(String[] args) {
 
-        Length l1 = new Length(value1, unit1);
-        Length l2 = new Length(value2, unit2);
+		
+		boolean result = demonstrateLengthComparison(1.0, Length.LengthUnit.YARDS, 36.0, Length.LengthUnit.INCHES);
 
-        System.out.println(l1 + " == " + l2 + " ? " + l1.equals(l2));
-    }
+		System.out.println("Are equal: " + result);
 
-    public static void demonstrateLengthConversion(Scanner sc) {
-        System.out.print("Enter value to convert: ");
-        double value = sc.nextDouble();
+	
+		Length converted = demonstrateLengthConversion(3.0, Length.LengthUnit.FEET, Length.LengthUnit.INCHES);
 
-        Length.LengthUnit fromUnit = getUnit(sc, "source");
-        Length.LengthUnit toUnit = getUnit(sc, "target");
+		System.out.println("Converted value: " + converted);
 
-        Length source = new Length(value, fromUnit);
-        Length converted = source.convertTo(toUnit);
+		Length yard = new Length(1.0, Length.LengthUnit.YARDS);
+		Length inches = demonstrateLengthConversion(yard, Length.LengthUnit.INCHES);
 
-        System.out.println(source + " -> " + converted);
-    }
-
-    public static Length.LengthUnit getUnit(Scanner sc, String label) {
-
-        System.out.println("Select " + label + " unit:");
-        System.out.println("1. FEET");
-        System.out.println("2. INCHES");
-        System.out.println("3. YARDS");
-        System.out.println("4. CENTIMETERS");
-
-        int choice = sc.nextInt();
-
-        switch (choice) {
-            case 1: return Length.LengthUnit.FEET;
-            case 2: return Length.LengthUnit.INCHES;
-            case 3: return Length.LengthUnit.YARDS;
-            case 4: return Length.LengthUnit.CENTIMETERS;
-            default: throw new IllegalArgumentException("Invalid Unit Choice");
-        }
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        demonstrateLengthComparison(sc);
-        demonstrateLengthConversion(sc);
-
-        sc.close();
-    }
+		System.out.println("1 yard in inches: " + inches);
+	}
 }
