3312b14d863f89591b536ac39ed1c3a48ae9cc40
riteshthakre1 <riteshthakre976@gmail.com>
2026-03-16 11:15:09 +0530

    [Ritesh] : modified QuantityMeasurementAppTest.java

--- src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/apps/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -1,42 +1,49 @@
 package com.apps.quantitymeasurement;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.apps.quantitymeasurement.QuantityMeasurementApp.Feet;
+import org.junit.jupiter.api.Test;
+
+import com.apps.quantitymeasurement.QuantityMeasurementApp.FeetEquality;
 
 public class QuantityMeasurementAppTest {
 
+	QuantityMeasurementApp.FeetEquality feet1;
+	QuantityMeasurementApp.FeetEquality feet2;
+
 	@Test
-	public void testFeetEquality_SameValue() {
-		Feet feet1 = new Feet(1.0);
-		Feet feet2 = new Feet(1.0);
-		assertTrue(feet1.equals(feet2), "Feet objects with same value should be equal");
+	public void testEquals() {
+		feet1 = new FeetEquality(1);
+		feet2 = new FeetEquality(1);
+		assertTrue(feet1.equals(feet2));
 	}
 
 	@Test
-	public void testFeetEquality_DifferentValue() {
-		Feet feet1 = new Feet(1.0);
-		Feet feet2 = new Feet(2.0);
-		assertFalse(feet1.equals(feet2), "Feet objects with different values should not be equal");
+	public void testDifferent() {
+		feet1 = new FeetEquality(1);
+		feet2 = new FeetEquality(2);
+		assertTrue(!feet1.equals(feet2));
 	}
 
 	@Test
-	public void testFeetEquality_NullComparison() {
-		Feet feet1 = new Feet(1.0);
-		assertFalse(feet1.equals(null), "Feet object should not be equal to null");
+	public void testNull() {
+		feet1 = new FeetEquality(1);
+		feet2 = null;
+		assertTrue(!feet1.equals(feet2));
 	}
 
 	@Test
-	public void testFeetEquality_DifferentClass() {
-		Feet feet1 = new Feet(1.0);
-		String notFeet = "1.0";
-		assertFalse(feet1.equals(notFeet), "Feet object should not be equal to a different class");
+	public void testSameRefferencet() {
+		feet1 = new FeetEquality(1);
+
+		assertTrue(feet1.equals(feet1));
 	}
 
 	@Test
-	public void testFeetEquality_SameReference() {
-		Feet feet1 = new Feet(1.0);
-		assertTrue(feet1.equals(feet1), "Feet object should be equal to itself (reflexive property)");
+	public void testEmptyVariable() {
+		feet1 = new FeetEquality(1);
+		feet2 = new FeetEquality();
+		assertTrue(!feet1.equals(feet2));
 	}
+
 }
