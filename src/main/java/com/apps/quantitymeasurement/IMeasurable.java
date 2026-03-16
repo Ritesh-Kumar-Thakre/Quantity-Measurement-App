a2dcd6a35b7de8291c2724f29493c9377048cb71
riteshthakre1 <riteshthakre976@gmail.com>
2026-03-16 12:48:23 +0530

    [Ritesh] : modified files

--- src/main/java/com/apps/quantitymeasurement/IMeasurable.java
+++ src/main/java/com/apps/quantitymeasurement/IMeasurable.java
@@ -1,12 +1,23 @@
-
 package com.apps.quantitymeasurement;
 
 public interface IMeasurable {
-	double getConversionFactor();
-
-	double convertToBaseUnit(double value);
-
-	double convertFromBaseUnit(double value);
-
-	String getUnitName();
+     double getConversionFactor();
+     
+     double convertToBaseUnit(double value);
+     
+     double convertFromBaseUnit(double value);
+     
+     String getUnitName();
+     
+     SupportArithemetic supportArithemetic = ()-> true;
+     
+     default boolean supportsArithemetics() {
+    	 return supportArithemetic.isSupported();
+     }
+     
+     default void validateOperationSupport(String operation){
+    	 if(!supportsArithemetics()) {
+    		 throw new UnsupportedOperationException("The Operation doest not supported "+operation);
+    	 }
+     }
 }
--- src/main/java/com/apps/quantitymeasurement/VolumneUnit.java
+++ src/main/java/com/apps/quantitymeasurement/VolumneUnit.java
@@ -1,11 +1,12 @@
-
 package com.apps.quantitymeasurement;
 
-public enum VolumneUnit implements IMeasurable {
-	LITRE(1.0), MILLILITRE(0.001), GALLON(3.78541);
-
+public enum VolumneUnit implements IMeasurable{
+      LITRE(1.0),
+      MILLILITRE(0.001),
+      GALLON(3.78541);
+	
 	private double conversion;
-
+	
 	VolumneUnit(double conversion) {
 		this.conversion = conversion;
 	}
@@ -17,19 +18,19 @@ public enum VolumneUnit implements IMeasurable {
 
 	@Override
 	public double convertToBaseUnit(double value) {
-		return value * this.getConversionFactor();
+		return value*this.getConversionFactor();
 	}
 
 	@Override
 	public double convertFromBaseUnit(double value) {
-
-		return (VolumneUnit.LITRE.getConversionFactor() * value) / this.getConversionFactor();
+		
+		return (VolumneUnit.LITRE.getConversionFactor()*value)/this.getConversionFactor();
 	}
 
 	@Override
 	public String getUnitName() {
-
+		
 		return VolumneUnit.this.name();
 	}
-
+      
 }
