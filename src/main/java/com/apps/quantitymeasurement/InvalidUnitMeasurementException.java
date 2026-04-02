d104971ab6f058b22d40f8a64b69685bf8127f53
riteshthakre1 <riteshthakre976@gmail.com>
2026-04-02 14:23:53 +0530

    [Ritesh] : update project to spring starter project

--- src/main/java/com/apps/quantitymeasurement/InvalidUnitMeasurementException.java
+++
@@ -1,7 +0,0 @@
-package com.apps.quantitymeasurement;
-
-public class InvalidUnitMeasurementException extends Exception {
-   public InvalidUnitMeasurementException(String str) {
-	   super(str);
-   }
-}
--- src/main/java/com/apps/quantitymeasurement/unit/SupportArithemetic.java
+++
@@ -1,6 +0,0 @@
-package com.apps.quantitymeasurement.unit;
-
-@FunctionalInterface
-interface SupportArithemetic {
-	boolean isSupported();
-}
