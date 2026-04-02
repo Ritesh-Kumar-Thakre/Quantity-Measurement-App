f6e9d3e6dbadc3dc6e5291a1db578449a417a336
riteshthakre1 <riteshthakre976@gmail.com>
2026-04-02 15:02:20 +0530

    [Ritesh] : added AuthenticationException.java in exception layer

---
+++ src/main/java/com/app/quantitymeasurementapp/exception/AuthenticationException.java
@@ -0,0 +1,13 @@
+package com.app.quantitymeasurementapp.exception;
+
+public class AuthenticationException extends Exception{
+	
+     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	 public AuthenticationException(String str) {
+    	 super(str);
+     }
+}
